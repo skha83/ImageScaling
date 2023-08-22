635b7d358fec7736c628ec710b4429a96eddcb98
Søren Kistrup Hansen <skha@uneeg.com>
2023-08-22 11:02:47 +0200

    Sample project to demonstrate MAUI iOS image downscaling problem.

---
+++ ImageScaling/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ImageScaling
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ImageScaling/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageScaling
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
